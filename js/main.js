a01bb1df75cc0bff01ce1a7846e8e68bcd9a8d69
Leonid <leonid.domahalskyy@stud.hs-flensburg.de>
2023-10-24 23:03:38 +0200

    capabilities

--- js/main.js
+++ js/main.js
@@ -1,68 +1,71 @@
 const btnDarkMode = document.querySelector(".dark-mode-btn");
 
 // Checking for dark theme at the system settings level
-if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches ) {
-    btnDarkMode.classList.add("dark-mode-btn--active");
-	document.body.classList.add("dark");
+if (
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: dark)").matches
+) {
+  btnDarkMode.classList.add("dark-mode-btn--active");
+  document.body.classList.add("dark");
 }
 
 //  Checking for dark theme in localStorage
-if (localStorage.getItem('darkMode') === 'dark') {
-    btnDarkMode.classList.add("dark-mode-btn--active");
-    document.body.classList.add("dark");
+if (localStorage.getItem("darkMode") === "dark") {
+  btnDarkMode.classList.add("dark-mode-btn--active");
+  document.body.classList.add("dark");
 } else if (localStorage.getItem("darkMode") === "light") {
-    btnDarkMode.classList.remove("dark-mode-btn--active");
-    document.body.classList.remove("dark");
+  btnDarkMode.classList.remove("dark-mode-btn--active");
+  document.body.classList.remove("dark");
 }
 
 //  If system settings change, update the theme
 window
-    .matchMedia("(prefers-color-scheme: dark)")
-    .addEventListener("change", (event) => {
-        const newColorScheme = event.matches ? "dark" : "light";
+  .matchMedia("(prefers-color-scheme: dark)")
+  .addEventListener("change", (event) => {
+    const newColorScheme = event.matches ? "dark" : "light";
 
-        if (newColorScheme === "dark") {
-			btnDarkMode.classList.add("dark-mode-btn--active");
-			document.body.classList.add("dark");
-			localStorage.setItem("darkMode", "dark");
-		} else {
-			btnDarkMode.classList.remove("dark-mode-btn--active");
-			document.body.classList.remove("dark");
-			localStorage.setItem("darkMode", "light");
-		}
-    });
+    if (newColorScheme === "dark") {
+      btnDarkMode.classList.add("dark-mode-btn--active");
+      document.body.classList.add("dark");
+      localStorage.setItem("darkMode", "dark");
+    } else {
+      btnDarkMode.classList.remove("dark-mode-btn--active");
+      document.body.classList.remove("dark");
+      localStorage.setItem("darkMode", "light");
+    }
+  });
 
 // Enabling dark mode through the button
 btnDarkMode.onclick = function () {
-    btnDarkMode.classList.toggle("dark-mode-btn--active");
-    const isDark = document.body.classList.toggle("dark");
+  btnDarkMode.classList.toggle("dark-mode-btn--active");
+  const isDark = document.body.classList.toggle("dark");
 
-    if (isDark) {
-        localStorage.setItem("darkMode", "dark");
-    } else {
-        localStorage.setItem("darkMode", "light");
-    }
+  if (isDark) {
+    localStorage.setItem("darkMode", "dark");
+  } else {
+    localStorage.setItem("darkMode", "light");
+  }
 };
 
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-list");
 
 hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-}) 
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
 
-document.querySelectorAll(".nav-list__item").forEach(n => n.addEventListener("click", () => {
+document.querySelectorAll(".nav-list__item").forEach((n) =>
+  n.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
-}))
-
+  })
+);
 
 /* marquee */
 
 const sections = document.querySelectorAll("section");
 
 [...sections].forEach((section) => {
-  section.classList.add("enable-animation"); // Füge standardmäßig die Klasse hinzu
+  section.classList.add("enable-animation");
 });
-
